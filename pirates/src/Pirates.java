f67cd2e2fefe431df1d6b53da0cfcb81bc36229e
Máté <dontirikk@gmail.com>
2017-04-10 15:32:42 +0200

    move main method to separate class, refactor code, fix class names

--- pirates/src/Pirates.java
+++ pirates/src/Pirates.java
@@ -1,40 +1,19 @@
 import java.util.*;
 
-public class PirateCounter{
+class Pirates {
+  String name;
+  boolean hasWoodenLeg;
+  int gold;
 
-  class Pirate {
-    String name;
-    boolean hasWoodenLeg;
-    int gold;
-
-    Pirate(String name, boolean hasWoodenLeg, int gold) {
-      this.name = name;
-      this.hasWoodenLeg = hasWoodenLeg;
-      this.gold = gold;
-    }
+  Pirates(String name, boolean hasWoodenLeg, int gold) {
+    this.name = name;
+    this.hasWoodenLeg = hasWoodenLeg;
+    this.gold = gold;
   }
 
-  public static void main(String... args){
-    ArrayList<Pirate> pirates = new ArrayList<>();
-
-    // Given this list...
-
-    pirates.add(new Pirate("Olaf", false, 12);
-    pirates.add(new Pirate("Uwe", true, 9);
-    pirates.add(new Pirate("Jack", true, 16);
-    pirates.add(new Pirate("Morgan", false, 17);
-    pirates.add(new Pirate("Hook", true, 20);
-
-    // Write a function that takes any list that contains pirates as in the example,
-    // And returns a list of names containing the pirates that
-    // - have wooden leg and
-    // - have more than 15 gold
-
-  }
-
-  private static List<String> WhosPegLeggedAndRich(List<Pirate> listOfPirates) {
+  static List<String> whosPegLeggedAndRich(List<Pirates> listOfPirates) {
     List<String> listToOutput = new ArrayList<>();
-    for (Pirate  pirate : listOfPirates) {
+    for (Pirates pirate : listOfPirates) {
       if (pirate.hasWoodenLeg && pirate.gold > 15) {
         listToOutput.add(pirate.name);
       }
@@ -42,3 +21,4 @@ public class PirateCounter{
     return listToOutput;
   }
 }
+
