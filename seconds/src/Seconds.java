e44993889676218f8c18e6f363b5d91f6f91226c
Máté <dontirikk@gmail.com>
2017-04-10 14:27:47 +0200

    move Second.java to /src, and import classes

--- seconds/src/Seconds.java
+++ seconds/src/Seconds.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Seconds {
   public static void main(String[] args) {
     // Create a function that takes a list as a parameter,
     // and returns a new list with every second element from the orignal list
     // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
+  }
+
+  public static List<Integer> everySecond(List<Integer> initList) {
+    List<Integer> newList = new ArrayList<Integer>();
+
   }
 }
