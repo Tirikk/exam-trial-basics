9bea96e57b677eaac1569abeda19bb7eaf09a6c1
Máté <dontirikk@gmail.com>
2017-04-10 15:51:06 +0200

    create Main class with main method

---
+++ box/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    Cuboid cuboid = new Cuboid(2, 3, 4);
+
+    System.out.println(cuboid.getSurface());
+    System.out.println(cuboid.getVolume());
+  }
+}
