75dc2fffa30ad88d70b92cf126ab59016a37e3ab
Máté <dontirikk@gmail.com>
2017-04-10 14:40:16 +0200

    create 'everySecond' function

--- seconds/src/Seconds.java
+++ seconds/src/Seconds.java
@@ -6,10 +6,14 @@ public class Seconds {
     // Create a function that takes a list as a parameter,
     // and returns a new list with every second element from the orignal list
     // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
+
   }
 
   public static List<Integer> everySecond(List<Integer> initList) {
-    List<Integer> newList = new ArrayList<Integer>();
-
+    List<Integer> newList = new ArrayList<>();
+    for (int i = 1; i < initList.size(); i += 2) {
+      newList.add(initList.get(i));
+    }
+    return newList;
   }
 }
